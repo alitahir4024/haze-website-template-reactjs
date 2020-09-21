20b9baa4521c792646e941820712729524912a1b
Ali-Tahir4024 <alitahir4024@gmail.com>
2020-09-22 02:09:55 +0500

    Updates

---
+++ precache-manifest.8e49733481dd656acedfb2abd471d928.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff04d68778e31fb2d5ae33fd79cdbb43",
+    "url": "/haze-website-template-reactjs/index.html"
+  },
+  {
+    "revision": "2904706a963a07082782",
+    "url": "/haze-website-template-reactjs/static/css/main.1d9d053a.chunk.css"
+  },
+  {
+    "revision": "7c66bba5fe03239bf2d2",
+    "url": "/haze-website-template-reactjs/static/js/2.e9e9a0da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/haze-website-template-reactjs/static/js/2.e9e9a0da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2904706a963a07082782",
+    "url": "/haze-website-template-reactjs/static/js/main.49891825.chunk.js"
+  },
+  {
+    "revision": "b33e2e240ba72fd528dc",
+    "url": "/haze-website-template-reactjs/static/js/runtime-main.6c7674e3.js"
+  },
+  {
+    "revision": "f53f90b10c50c8caf684003efd70833f",
+    "url": "/haze-website-template-reactjs/static/media/about-us.f53f90b1.jpg"
+  },
+  {
+    "revision": "6d009055d0293548fe79037e2732f118",
+    "url": "/haze-website-template-reactjs/static/media/hero-bg.6d009055.jpg"
+  },
+  {
+    "revision": "d878b6359086a0b846613e7f5a25c7f5",
+    "url": "/haze-website-template-reactjs/static/media/member-1.d878b635.jpg"
+  },
+  {
+    "revision": "a808b7c863811ddf9f65d7c5c5873cc3",
+    "url": "/haze-website-template-reactjs/static/media/member-2.a808b7c8.jpg"
+  },
+  {
+    "revision": "79020031f63c52e5652936ace9a54c1c",
+    "url": "/haze-website-template-reactjs/static/media/member-3.79020031.jpg"
+  },
+  {
+    "revision": "145078dd718027b92aec794077f2ec55",
+    "url": "/haze-website-template-reactjs/static/media/portfolio-1.145078dd.jpg"
+  },
+  {
+    "revision": "cd1042b98e839ea36dd2902e3c77173c",
+    "url": "/haze-website-template-reactjs/static/media/portfolio-2.cd1042b9.jpg"
+  },
+  {
+    "revision": "bb9ca2d88f58cc1712aca52535087321",
+    "url": "/haze-website-template-reactjs/static/media/portfolio-3.bb9ca2d8.jpg"
+  },
+  {
+    "revision": "9b29b09257b6a0154a2b7058d0753d81",
+    "url": "/haze-website-template-reactjs/static/media/portfolio-4.9b29b092.jpg"
+  }
+]);
